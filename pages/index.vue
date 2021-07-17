d298c4f74b193a291b6bf48916fbda11dcfadf3b
Marcin Wosinek <marcin@wosinek.eu>
2021-07-17 20:55:23 +0200

    replace tutorial with pdf-lib

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,32 @@
 <template>
-  <Tutorial/>
+  <div>
+    <button v-on:click="generatePdf">Generate pdf</button>
+
+    <br />
+
+    <iframe style="width: 350px; height: 600px" v-bind:src="pdf"></iframe>
+  </div>
 </template>
 
 <script>
-export default {}
+const { PDFDocument } = require("pdf-lib");
+
+export default {
+  data() {
+    return {
+      pdf: "",
+    };
+  },
+
+  methods: {
+    async generatePdf() {
+      const pdfDoc = await PDFDocument.create();
+      const page = pdfDoc.addPage([350, 400]);
+      page.moveTo(110, 200);
+      page.drawText("Hello World!");
+      const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+      this.pdf = pdfDataUri;
+    }
+  }
+};
 </script>
